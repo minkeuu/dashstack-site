9f505083c4aaecb11890d49d3087edb2a970f26f
Матвей <tretakovmatvey@gmail.com>
2026-04-03 21:56:36 +0300

    fix rating in prodCard

--- my-app/src/components/product/Rating.jsx
+++ my-app/src/components/product/Rating.jsx
@@ -1,12 +1,13 @@
 const stars = [1, 2, 3, 4, 5];
-
+import Star from "../../assets/misc/Star.svg";
+import GrayStar from "../../assets/misc/Gray Star.svg";
 export default function Rating({ rating, number }) {
   return (
     <div className="flex items-center gap-1">
       {stars.map((num, i) => (
         <img
           key={i}
-          src={num <= rating ? "./src/assets/misc/Star.svg" : "./src/assets/misc/Gray Star.svg"}
+          src={num <= rating ? Star : GrayStar}
           alt=""
           className="w-4 h-4"
         />
